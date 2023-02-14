94101457c6e33e94158e8b732169de162f42c905
Gustav72 <wolfgangkirstein7@gmail.com>
2023-02-13 21:45:03 -0500

    combine components

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,20 @@
 import React, { Component } from 'react';
 import uniqid from 'uniqid';
-import Overview from './components/Overview';
+import GeneralInfo from './GeneralInfo';
+import Experience from './Experience';
+import Education from './Education'
 
 class App extends Component {
-  
-  constructor() {
-    super();
-    
-    this.state = {
-      task: { 
-        text: '',
-        id: uniqid()
-      },
-      tasks: [],
-    };
-  }
-  
-  handleChange = (e) => {
-    this.setState({
-      task: {
-        text: e.target.value,
-        id: this.state.task.id,
-      }
-    })
-  }
-
-  onSubmitTask = (e) => {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
-      task: { 
-        text: '',
-      id: uniqid()
-    },
-    })
-  }
 
   render() { 
-    const { task, tasks } = this.state;
 
     return (
       <div>
-        <form onSubmit={this.onSubmitTask}>
-          <label htmlFor="taskInput">Enter Task</label>
-          <input onChange={this.handleChange} value={task.text} type="text" id="taskInput"/>
-          <button type="submit">Add Task</button>
-        </form>
-        <Overview tasks={tasks} />
+        <h1>CV Creator</h1>
+        <GeneralInfo />
+        <Experience />
+        <Education />
+        <button>Print to PDF</button>
       </div>
     );
   }
