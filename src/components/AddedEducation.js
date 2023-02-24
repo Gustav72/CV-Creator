ce8e39a044446b1185eaed7583ebd4c4b4eedaa6
Gustav72 <wolfgangkirstein7@gmail.com>
2023-02-23 21:25:53 -0500

    remove unneeded comments

--- src/components/AddedEducation.js
+++ src/components/AddedEducation.js
@@ -29,19 +29,6 @@ const AddedEducation = (props) => {
                 )
             })}
         </div>
-
-        // <ul>
-        //     {allEducation.map((education) => {
-        //         return (
-        //             <div key={education.id}>
-        //                 <li key={education.id + '_institution'}>{education.institution}</li>
-        //                 <li key={education.id + '_fieldOfFocus'}>{education.fieldOfFocus}</li>
-        //                 <li key={education.id + '_dateFrom'}>{education.dateFrom}</li>
-        //                 <li key={education.id + '_dateTo'}>{education.dateTo}</li>
-        //             </div>
-        //         )
-        //     })}
-        // </ul>
     )
 }
 
