c1ee979d3dc9ab168320ccce16e6ea85feef9750
Gustav72 <wolfgangkirstein7@gmail.com>
2023-02-25 09:28:58 -0500

    add editing, displaying, and saving features

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -5,15 +5,13 @@ class GeneralInfo extends Component {
     
     constructor(props) {
         super(props);
-
         this.state = {
             firstName: '',
             lastName: '',
             email: '',
             phoneNumber: '',
-            buttonText: 'Save',
+            isEditing: true,
         };
-        this.handleClick = this.handleClick.bind(this);
     }
 
     handleFirstNameChange = (event) => {
@@ -40,41 +38,60 @@ class GeneralInfo extends Component {
         })
     }
 
-    handleClick = () => {
-        this.setState((prevState) => ({
-            buttonText: prevState.buttonText === 'Save' ? 'Edit' : 'Save'
-        }));
+    handleSaveClick = () => {
+        this.setState({
+            isEditing: false
+        });
     }
 
-
+    handleEditClick = () => {
+        this.setState({
+            isEditing: true
+        })
+    }
 
     render() {
         return (
             <div className="general-info">
                 <h3>General Info</h3>
+                { this.state.isEditing ? (
+                    <div>
+                        <div>
+                            <div>
+                                <label>First Name:</label>
+                                <input type="text" value={this.state.firstName} onChange={this.handleFirstNameChange}></input>
+                            </div>
+                            <div>
+                                <label>Last Name:</label>
+                                <input type="text" value={this.state.lastName} onChange={this.handleLastNameChange}></input>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <label>Email:</label>
+                                <input type="email" value={this.state.stateEmail} onChange={this.handleEmailChange}></input>
+                            </div>
+                            <div>
+                                <label>Phone #:</label>
+                                <input type="number" value={this.state.phoneNumber} onChange={this.handlePhoneNumberChange}></input>
+                            </div>
+                        </div>
+                        <button id="add-education" onClick={this.handleSaveClick}>Save</button>
+                    </div>
+                ) : (
                 <div>
                     <div>
-                        <label>First Name:</label>
-                        <input type="text" value={this.state.firstName} onChange={this.handleFirstNameChange}></input>
+                        <p>{this.state.firstName}</p>
+                        <p>{this.state.lastName}</p>
                     </div>
                     <div>
-                        <label>Last Name:</label>
-                        <input type="text" value={this.state.lastName} onChange={this.handleLastNameChange}></input>
+                        <p>{this.state.email}</p>
+                        <p>{this.state.phoneNumber}</p>
                     </div>
+                    <button id="add-education" onClick={this.handleEditClick}>Edit</button>
                 </div>
-                <div>
-                    <div>
-                        <label>Email:</label>
-                        <input type="email" value={this.state.stateEmail} onChange={this.handleEmailChange}></input>
-                    </div>
-                    <div>
-                        <label>Phone #:</label>
-                        <input type="number" value={this.state.phoneNumber} onChange={this.handlePhoneNumberChange}></input>
-                    </div>
-                </div>
-                <button id="add-education" type="submit" onClick={this.handleClick}>{this.state.buttonText}</button>
+                    )}
             </div>
-        )
+        );
     }
-
 }
